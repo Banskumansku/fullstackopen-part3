987a252162ade891f70f88130e5e3dd4be1ddf1a
Banskumansku <jan.lindholm@helsinki.fi>
2020-09-10 12:35:05 +0300

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan  = require('morgan')
-const app = express()
+const cors = require('cors')
 
+const app = express()
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
@@ -79,7 +81,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
